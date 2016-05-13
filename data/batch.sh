786bc03cadfb4470329ab297a3844ef47b70e7fa
228miles <hangdothiduc@gmail.com>
2016-05-13 01:49:58 -0400

    add visual explorations

--- data/batch.sh
+++ data/batch.sh
@@ -2,7 +2,7 @@
 echo hello world
 for file in full/*.jpg
 do
-    ffmpeg -i "$file" -vf scale=1280:-1 medium1280/$(basename "${file/.jpg}").jpg
+    ffmpeg -i "$file" -vf scale=1280:-1 small960/$(basename "${file/.jpg}").jpg
 done
 
 # for another time to extract one frame every shot!
